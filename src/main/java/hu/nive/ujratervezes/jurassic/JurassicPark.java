6e7516a9f01982a1c7e719cf41fbf9a486bfef7e
Adam Hamar <adam.hamar@gmail.com>
2021-04-29 12:05:07 +0200

    Implement the JurassicPark class

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,15 +1,36 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
-    private String dbUrl;
-    private String dbUser;
-    private String dbPassword;
+    private final String dbUrl;
+    private final String dbUser;
+    private final String dbPassword;
 
     public JurassicPark(String dbUrl, String dbUser, String dbPassword) {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
     }
-
+    
+    public List<String> checkOverpopulation() {
+        List<String> breeds = new ArrayList<>();
+        
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String sql = "SELECT breed FROM dinosaur WHERE actual > expected ORDER BY breed;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            while (resultSet.next()) {
+                breeds.add(resultSet.getString("breed"));
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        
+        return breeds;
+    }
 }
